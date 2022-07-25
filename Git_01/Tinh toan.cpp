37e609e54bdce70c701101a9ddd1df06bacdb77b
An Teemo <an.hnt12250@sinhvien.hoasen.edu.vn>
2022-07-25 15:34:52 +0700

    Hoan thien file Tinh toan.cpp

--- Git_01/Tinh toan.cpp
+++ Git_01/Tinh toan.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main () {
-    cout << "Tinh tong cua A va B0";
-    
+    cout << "Tinh tong cua A va B";
+
+    //Update
+    int a, b;
+    cout << "Tong cua A va B la: " << a+b;
+
     return 0;
 }
